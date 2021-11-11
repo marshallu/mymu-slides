0570793b9af913d13f3dfa7611f4b8eb237a73a7
Christopher McComas <cmccomas@marshall.edu>
2021-11-11 08:08:46 -0500

    add transient for slides and update name of plugin

--- my-mymu-slides.php
+++ my-mymu-slides.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * MyMU Slides
+ * MU MyMU Slides
  *
  * This plugin allows MyMU to pull in slides from WordPress
  *
  * @package mymu-slides
  *
- * Plugin Name: MyMU Slides
+ * Plugin Name: MU MyMU Slides
  * Plugin URI: https://www.marshall.edu
  * Description: A plugin allowing slides from MyMU to pull data from WordPress.
  * Version: 1.0
@@ -42,46 +42,52 @@ register_activation_hook( __FILE__, 'mymu_slides_deactivate' );
  */
 function mymu_slides_query() {
 
-	$args = array(
-		'post_type'      => 'attachment',
-		'post_status'    => 'inherit',
-		'posts_per_page' => 25,
-		'orderby'        => array(
-			'meta_value_num' => 'ASC',
-			'date'           => 'DESC',
-		),
-		'meta_key'       => 'order_by', // phpcs:ignore
-		'meta_query'     => array( // phpcs:ignore
-			'relation' => 'AND',
-			array(
-				'key'   => 'mymu_slides_add_to_mymu_slideshow', // phpcs:ignore
-				'value' => true, // phpcs:ignore
+	if ( false === get_transient( 'mu_mymu_slides' ) ) {
+		$args = array(
+			'post_type'      => 'attachment',
+			'post_status'    => 'inherit',
+			'posts_per_page' => 25,
+			'orderby'        => array(
+				'meta_value_num' => 'ASC',
+				'date'           => 'DESC',
 			),
-			array(
-				'key'     => 'mymu_slides_expire_date',
-				'value'   => date( 'Y-m-d h:i:s' ), // phpcs:ignore
-				'type'    => 'DATETIME',
-				'compare' => '>',
+			'meta_key'       => 'order_by', // phpcs:ignore
+			'meta_query'     => array( // phpcs:ignore
+				'relation' => 'AND',
+				array(
+					'key'   => 'mymu_slides_add_to_mymu_slideshow', // phpcs:ignore
+					'value' => true, // phpcs:ignore
+				),
+				array(
+					'key'     => 'mymu_slides_expire_date',
+					'value'   => date( 'Y-m-d h:i:s' ), // phpcs:ignore
+					'type'    => 'DATETIME',
+					'compare' => '>',
+				),
 			),
-		),
-	);
+		);
 
-	$the_query = new WP_Query( $args );
+		$the_query = new WP_Query( $args );
 
-	if ( $the_query->have_posts() ) {
-		$data = array();
+		if ( $the_query->have_posts() ) {
+			$data = array();
 
-		while ( $the_query->have_posts() ) {
-			$the_query->the_post();
-			$data[] = array(
-				'title'    => get_the_ID(),
-				'imageUrl' => wp_get_attachment_url(),
-				'url'      => get_field( 'mu_gallery_external_url' ),
-			);
+			while ( $the_query->have_posts() ) {
+				$the_query->the_post();
+				$data[] = array(
+					'title'    => get_the_ID(),
+					'imageUrl' => wp_get_attachment_url(),
+					'url'      => get_field( 'mu_gallery_external_url' ),
+				);
+			}
 		}
-	}
 
-	wp_reset_postdata();
+		wp_reset_postdata();
+
+		set_transient( 'mu_mymu_slides', $data, 43000 );
+	} else {
+		$data = get_transient( 'mu_mymu_slides' );
+	}
 
 	return $data;
 }
@@ -100,3 +106,18 @@ add_action(
 		);
 	}
 );
+
+/**
+ * Delete the transient for slides when post is updated.
+ *
+ * @param string $new The string of the new post type.
+ * @param string $old The string of the old post type.
+ * @param object $post The post object.
+ * @return void
+ */
+function mu_mymu_slides_purge_transient_on_publish( $new, $old, $post ) {
+	if ( 'publish' === $new ) {
+		delete_transient( 'mu_mymu_slides' );
+	}
+}
+add_action( 'transition_post_status', 'mu_mymu_slides_purge_transient_on_publish', 10, 3 );
